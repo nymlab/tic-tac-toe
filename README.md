37d87aca2bbc073af50243b7e7a96bedafd65bb5
bwty <whalelephant@users.noreply.github.com>
2023-06-15 18:07:18 +0800

    stargaze

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - uni-6: code_id = 2337, addr = juno1tsqeaanlxv5wlu0zdcje2lute0m8g0nszz3gwatr0tyauujskrws55h09u
 - pion-1: code_id = 709, addr = neutron1h92cjyqnh8x6t4evvsanpuh93jspjl764zdqkj72cemlcs05kdlsqkwvs5
+- elgarfa-1: code_id = 2562, addr = stars1lshpwv5ee4krqvvpksv2a9w70u6hjkvsa9t6vv2lqespxr29m4asd3sz5q
--- upload-instantiate.sh
+++ upload-instantiate.sh
@@ -10,39 +10,40 @@ if [[ "$NETWORK" == "juno_testnet" ]]; then
   DAEMON=junod 
 elif [[ "$NETWORK" == "neutron_testnet" ]]; then
   DAEMON=neutrond
+elif [[ "$NETWORK" == "stargaze_testnet" ]]; then
+  DAEMON=starsd
 fi 
 
 CHAIN_FLAGS="--chain-id $CHAIN_ID --node $RPC"
 
 
-# echo "Add key to keyring - $DAEMON"
-# if ! $DAEMON keys show deployer --keyring-backend=test; then
-#   (
-#     echo "$UPLOAD_USER_MNEMONICS"
-#     echo "$UPLOAD_USER_MNEMONICS"
-#   ) | $DAEMON keys add deployer --recover --keyring-backend=test 
-# fi
+ #echo "Add key to keyring - $DAEMON"
+ #if ! $DAEMON keys show deployer --keyring-backend=test; then
+ #  (
+ #    echo "$UPLOAD_USER_MNEMONICS"
+ #    echo "$UPLOAD_USER_MNEMONICS"
+ #  ) | $DAEMON keys add deployer --recover --keyring-backend=test 
+ #fi
 
 
-# PATH_TO_CONTRACT_WASM=artifacts/tic_tac_toe.wasm
-# REST=$($DAEMON tx wasm store $PATH_TO_CONTRACT_WASM --gas auto --gas-prices 0.025$FEE_TOKEN --gas-adjustment 1.3 --from deployer --keyring-backend test  $CHAIN_FLAGS -o json -y)
-# echo $REST
-# 
-# sleep 15
-# 
-# RESQ=$($DAEMON q tx --type=hash $(echo "$REST"| jq -r '.txhash') $CHAIN_FLAGS -o json)
-# CODE_ID=$(echo "$RESQ" | jq -r '.logs[0].events[]| select(.type=="store_code").attributes[]| select(.key=="code_id").value')
-# CODE_HASH=$(echo "$RESQ" | jq -r '.logs[0].events[]| select(.type=="store_code").attributes[]| select(.key=="code_checksum").value')
-# 
-# echo "Code Id:  $CODE_ID"
-# 
-# echo "\n Instantiate Contract \n"
-# 
-# INIT_MSG='{}'
-# $DAEMON tx wasm instantiate $CODE_ID "$INIT_MSG" --gas auto --gas-prices 0.025$FEE_TOKEN --gas-adjustment 1.3 --from deployer --label 'Vectis TicTacToe' --keyring-backend test $CHAIN_FLAGS -o json -y --no-admin
-# 
-# sleep 15
-CODE_ID=709
+ PATH_TO_CONTRACT_WASM=artifacts/tic_tac_toe.wasm
+ REST=$($DAEMON tx wasm store $PATH_TO_CONTRACT_WASM --gas auto --gas-prices 0.025$FEE_TOKEN --gas-adjustment 1.3 --from deployer --keyring-backend test  $CHAIN_FLAGS -o json -y)
+ echo $REST
+ 
+ sleep 15
+ 
+ RESQ=$($DAEMON q tx --type=hash $(echo "$REST"| jq -r '.txhash') $CHAIN_FLAGS -o json)
+ CODE_ID=$(echo "$RESQ" | jq -r '.logs[0].events[]| select(.type=="store_code").attributes[]| select(.key=="code_id").value')
+ CODE_HASH=$(echo "$RESQ" | jq -r '.logs[0].events[]| select(.type=="store_code").attributes[]| select(.key=="code_checksum").value')
+ 
+ echo "Code Id:  $CODE_ID"
+ 
+ echo "\n Instantiate Contract \n"
+ 
+ INIT_MSG='{}'
+ $DAEMON tx wasm instantiate $CODE_ID "$INIT_MSG" --gas auto --gas-prices 0.025$FEE_TOKEN --gas-adjustment 1.3 --from deployer --label 'Vectis TicTacToe' --keyring-backend test $CHAIN_FLAGS -o json -y --no-admin
+ 
+ sleep 15
 
 CONTRACT_ADDRESS=$($DAEMON query wasm list-contract-by-code $CODE_ID $CHAIN_FLAGS --output json | jq -r '.contracts[-1]')
 echo "CONTRACT_ADDRESS : $CONTRACT_ADDRESS"
